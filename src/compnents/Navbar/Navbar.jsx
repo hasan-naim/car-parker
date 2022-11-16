e36ee98a227acbc50cc414f2042b00dbf7551864
hasan-naim <hasannaim6246@gmail.com>
2022-11-16 10:40:14 +0600

    navbar is done

--- src/compnents/Navbar/Navbar.jsx
+++ src/compnents/Navbar/Navbar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="bg-white sticky top-0 shadow-md">
+      <div className="container">
+        <div className="navbar">
+          <div className="flex-1">
+            <Link
+              to={"/"}
+              className="normal-case text-2xl md:text-4xl logo hover:scale-[1.1] duration-500"
+            >
+              Car Parker
+            </Link>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <Link
+                  className="hover:bg-transparent hover:text-primary focus:bg-transparent font-medium text-gray-500"
+                  to={"/"}
+                >
+                  Home
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className="hover:bg-transparent hover:text-primary focus:bg-transparent font-medium text-gray-500"
+                  to={"/park"}
+                >
+                  Park
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
